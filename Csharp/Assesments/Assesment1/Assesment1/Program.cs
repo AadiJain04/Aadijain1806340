89c9b3cc1043e4a6037122e4012f7a67da741a04
Aadijain1806340 <aadij@infinite.com>
2024-11-06 10:53:13 +0530

    Completed assesment

---
+++ Csharp/Assesments/Assesment1/Assesment1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------Answer 1--------------------------");
+            Console.WriteLine(check("Python", 1)); 
+            Console.WriteLine(check("Python", 0)); 
+            Console.WriteLine(check("Python", 4));
+
+            ques2();
+            ques3();
+            ques4();
+            Console.ReadLine();                  
+        }
+        public static string check(string a, int n)
+        {
+            return a.Remove(n, 1);
+        }
+
+        public static void ques2()
+        {
+            Console.WriteLine("--------------------Answer 2--------------------------");
+
+            Console.WriteLine("Enter a string:");
+            string inputString = Console.ReadLine();
+
+              string swapString = SwapLetters(inputString);
+
+            Console.WriteLine("Swapped string: " + swapString);
+
+            }
+            public static string SwapLetters(string input)
+            {
+                if (input.Length <= 1)
+                {
+                    return input;
+                }
+
+                char[] charArr = input.ToCharArray();
+                char temp = charArr[0];
+                charArr[0] = charArr[charArr.Length - 1];
+                charArr[charArr.Length - 1] = temp;
+
+                return new string(charArr);
+            } 
+
+
+        public static void ques3()
+        {
+            Console.WriteLine("--------------------Answer 3--------------------------");
+            Console.WriteLine("Enter 3 integers :");
+            string input = Console.ReadLine();
+
+            string[] numbers = input.Split(',');
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+            int num3 = int.Parse(numbers[2]);
+
+            int largest = Largest(num1, num2, num3);
+
+            Console.WriteLine("The largest number is: " + largest);
+        }
+        public static int Largest(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+            {
+                return a;
+            }
+            else if (b >= a && b >= c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        public static void ques4()
+        {
+            Console.WriteLine("--------------------Answer 4--------------------------");
+
+            Console.Write("Enter a string: ");
+            string word = Console.ReadLine();
+            Console.Write("Enter the letter to count: ");
+            char letter = Console.ReadKey().KeyChar;
+
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.ToLower(word[i]) == char.ToLower(letter))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine( letter + "' appears " + count + " times.");
+        }
+    }
+}
---
+++ Csharp/Assignments/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+
+            Console.WriteLine("Find the length of a string:");
+
+            Console.WriteLine("Input the string: ");
+            x = Console.ReadLine();
+
+            int len = x.Length;
+
+            Console.WriteLine("Length of the string is: {0}", len);
+
+            ques2();
+            ques3();
+            Console.ReadLine();
+        }
+
+        static void ques2()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Reverse of a string");
+            string x = Console.ReadLine();
+            char[] ch = x.ToCharArray();
+            Array.Reverse(ch);
+            string res = new string(ch);
+
+            Console.WriteLine("Reversed String "+ res);
+        }
+
+        static void ques3()
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("If words are equal or not");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            if (a == b) {
+                Console.WriteLine("Both the words are equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+        }  
+        
+        //--------------INHERITANCE------------------
+
+
+
+    }
+}
