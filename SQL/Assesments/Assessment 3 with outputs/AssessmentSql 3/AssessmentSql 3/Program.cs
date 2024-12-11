3d403d2be49def07f293eda5b93f6238cac95d95
Aadijain1806340 <aadij@infinite.com>
2024-12-11 11:56:17 +0530

    Assessment 3 completed

---
+++ SQL/Assesments/Assessment 3 with outputs/AssessmentSql 3/AssessmentSql 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=ICS-LT-41YLV44\\SQLEXPRESS;Database=Assessment;Trusted_Connection=True;";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand command = new SqlCommand("InsertingPDetails", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@ProductName", "Anarc Watch");
+                command.Parameters.AddWithValue("@Price", 50000.00);
+
+                SqlParameter productIdParam = new SqlParameter("@GeneratedProductId", SqlDbType.Int){};
+                command.Parameters.Add(productIdParam);
+
+               
+                command.ExecuteNonQuery();
+
+                int generatedProductId = (int)productIdParam.Value;
+
+                Console.WriteLine($"Generated ProductId: {generatedProductId}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
